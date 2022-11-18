e7b7d5f6c276aefb00ae69fb91fbd4296da8b21b
andrewtanjaya <andrewtanjaya21@gmail.com>
2022-11-18 21:34:36 +0700

    Init Firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { useCollectionData } from 'react-firebase-hooks/firestore'
+import { usersRef } from './Database/Firebase';
+
 
 function App() {
+
+  const [allUser, isLoadingAllUser, isErrorAllUser] = useCollectionData(usersRef, {
+    idField: 'id',
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {allUser ? <pre>{JSON.stringify(allUser)}</pre> : <p>No Data</p>}
     </div>
   );
 }
