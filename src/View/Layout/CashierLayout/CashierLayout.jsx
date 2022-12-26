7ce0edbdd26752ca83c51786b0082998e4258493
andrewtanjaya <andrewtanjaya21@gmail.com>
2022-12-27 00:49:22 +0700

    fix ui

--- src/View/Layout/CashierLayout/CashierLayout.jsx
+++ src/View/Layout/CashierLayout/CashierLayout.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, Outlet, useNavigate } from "react-router-dom";
 import AuthConsumer from "../../../hooks/auth";
 import { Menu, Input, Space } from "antd";
@@ -7,9 +7,12 @@ import { RestaurantController } from "../../../Controller/RestaurantController";
 import { UserController } from "../../../Controller/UserController";
 import NavbarRestaurantProfile from "../../Component/NavbarRestaurantProfile/NavbarRestaurantProfile";
 import NavbarUserProfile from "../../Component/NavbarUserProfile/NavbarUserProfile";
+import "./CashierLayout.css";
+import { CloseOutlined, SearchOutlined } from "@ant-design/icons";
 
 function CashierLayout() {
   const data = JSON.parse(sessionStorage.getItem("userData"));
+  const [showSearch, setShowSearch] = useState(false);
   const { Search } = Input;
   const [user, userLoading, userError, userSnapshot] = useDocumentData(
     UserController.getUserProfileByEmail(data.email),
@@ -29,6 +32,16 @@ function CashierLayout() {
     return console.log();
   };
 
+  const [currentPath, setCurrentPath] = useState("/cashier");
+
+  useEffect(() => {
+    setCurrentPath(window.location.pathname);
+  }, []);
+
+  const changeActive = (url) => {
+    setCurrentPath(url);
+  };
+
   const renderNavbar = () => {
     return (
       <div className="admin-restaurant-navbar">
@@ -37,74 +50,114 @@ function CashierLayout() {
         ) : (
           <></>
         )}
-        <ul className="admin-navbar-item">
-          <li>
-            <Link
-              to="/admin"
-              style={{ textDecoration: "none", color: "black" }}
-              // onClick={() => changeActive("/admin")}
-              // className={
-              //   currentPath === "/admin" ? "active-nav" : ""
-              // }
+        <div className="navbar-item-container">
+          <div
+            className={
+              showSearch
+                ? "search-bar-navbar-container show-search-bar"
+                : "search-bar-navbar-container"
+            }
+          >
+            <input
+              className="search-input-cashier-navbar"
+              type="text"
+              placeholder="Search order or menu..."
+            />
+            <CloseOutlined
+              onClick={() => setShowSearch(false)}
+              className="close-search-navbar-icon"
+            />
+          </div>
+
+          <ul className="admin-navbar-item">
+            <li>
+              <Link
+                to="/cashier"
+                style={{ textDecoration: "none", color: "black" }}
+                onClick={() => changeActive("/cashier")}
+                className={currentPath === "/cashier" ? "active-nav" : ""}
+              >
+                Dashboard
+              </Link>
+            </li>
+            <li>
+              <Link
+                style={{ textDecoration: "none", color: "black" }}
+                onClick={() => changeActive("/cashier/menuAvailibility")}
+                className={
+                  currentPath === "/cashier/menuAvailibility"
+                    ? "active-nav"
+                    : ""
+                }
+                to="/cashier/menuAvailibility"
+              >
+                Menu
+              </Link>
+            </li>
+            <li>
+              <Link
+                style={{ textDecoration: "none", color: "black" }}
+                to="/cashier/transactionReport"
+                onClick={() => changeActive("/cashier/transactionReport")}
+                className={
+                  currentPath === "/cashier/transactionReport"
+                    ? "active-nav"
+                    : ""
+                }
+              >
+                Report
+              </Link>
+            </li>
+            <li>
+              <Link
+                style={{ textDecoration: "none", color: "black" }}
+                onClick={() => changeActive("/cashier/cart")}
+                to="/cashier/cart"
+                className={currentPath === "/cashier/cart" ? "active-nav" : ""}
+              >
+                Cart
+              </Link>
+            </li>
+            <li>
+              <Link
+                style={{ textDecoration: "none", color: "black" }}
+                to="/cashier/orders"
+                onClick={() => changeActive("/cashier/orders")}
+                className={
+                  currentPath === "/cashier/orders" ? "active-nav" : ""
+                }
+              >
+                Orders
+              </Link>
+            </li>
+            <li>
+              <Link
+                style={{ textDecoration: "none", color: "black" }}
+                to="/cashier/generateQrCode"
+                onClick={() => changeActive("/cashier/generateQrCode")}
+                className={
+                  currentPath === "/cashier/generateQrCode" ? "active-nav" : ""
+                }
+              >
+                QR Codes
+              </Link>
+            </li>
+            <li
+              onClick={() => setShowSearch(true)}
+              style={{ padding: 0, margin: "0 6px" }}
+              className="navbar-search-icon"
             >
-              Dashboard
-            </Link>
-          </li>
-          <li>
-            <Link
-              style={{ textDecoration: "none", color: "black" }}
-              // onClick={() => changeActive("/admin/food")}
-              // className={
-              //   currentPath === "/admin/food" ? "active-nav" : ""
-              // }
-              to="/admin/food"
+              <SearchOutlined className="search-icon-cashier-navbar" />
+            </li>
+          </ul>
+          <div
+              onClick={() => setShowSearch(true)}
+              style={{ padding: 0, margin: "0 6px" }}
+              className="navbar-search-icon-small"
             >
-              Menu
-            </Link>
-          </li>
-          <li>
-            <Link
-              style={{ textDecoration: "none", color: "black" }}
-              to="/admin/transactionReport"
-              // onClick={() => changeActive("/admin/transactionReport")}
-              // className={
-              //   currentPath === "/admin/transactionReport"
-              //     ? "active-nav"
-              //     : ""
-              // }
-            >
-              Report
-            </Link>
-          </li>
-          <li>
-            <Link
-              style={{ textDecoration: "none", color: "black" }}
-              // onClick={() => changeActive("/admin/employee")}
-              to="/admin/employee"
-              // className={
-              //   currentPath === "/admin/employee"
-              //     ? "active-nav"
-              //     : ""
-              // }
-            >
-              Employee
-            </Link>
-          </li>
-          <li>
-            <Link
-              style={{ textDecoration: "none", color: "black"  }}
-              to="/admin/setting"
-              // onClick={() => changeActive("/admin/setting")}
-              // className={
-              //   currentPath === "/admin/setting"
-              //     ? "active-nav"
-              //     : ""
-              // }
-            >
-              Restaurant
-            </Link>
-          </li>
-        </ul>
+              <SearchOutlined className="search-icon-cashier-navbar" />
+            </div>
+        </div>
         {!userLoading && user ? <NavbarUserProfile userData={user} /> : <></>}
       </div>
       // <nav>
